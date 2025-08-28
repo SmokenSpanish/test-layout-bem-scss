dde91fca37ddddf952105324f083da29a3b86cb7
Maksim <maksimbasov1992@mail.ru>
2025-08-28 23:33:34 +0700

    feat: header + burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 console.log('JS connected');
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('[data-burger]');
+  const nav = document.getElementById('nav');
+  console.log(burger)
+  console.log(nav)
+
+  burger.addEventListener('click', () => {
+    const expanded = burger.getAttribute('aria-expanded') === 'true';
+    burger.setAttribute('aria-expanded', !expanded);
+    nav.classList.toggle('is-active');
+  });
+});
